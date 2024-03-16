e4cb15635363d27c976e46397ece6c9805842ad4
Jenny Dinh <jennydinh94@gmail.com>
2024-03-16 16:16:27 +0100

    Update footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        hero: "url(/src/images/gothenburg.jpg)",
+        hero: "url(https://images.unsplash.com/photo-1575872058841-955be84be5e7?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
       },
     },
   },
